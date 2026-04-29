af590df299e21cb3feba721cc4173902ec43b338
Samir Jose Gil <144859085+SamirJoseGil@users.noreply.github.com>
2026-04-29 12:50:11 -0500

    update url backend

--- frontend/app/env.server.ts
+++ frontend/app/env.server.ts
@@ -1,6 +1,6 @@
 // app/env.server.ts
 export const ENV = {
-  API_BASE_URL: process.env.api_base_url ?? process.env.API_BASE_URL ?? "http://127.0.0.1:8000/api",
+  API_BASE_URL: process.env.api_base_url ?? process.env.API_BASE_URL ?? "/api",
   NODE_ENV: process.env.NODE_ENV ?? "development",
 };
 
